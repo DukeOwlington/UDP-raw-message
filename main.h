591a4a827d8247f57c015b0a11026d9fa699da95
SugarKeyboard <workmanmail84@gmail.com>
2017-03-23 18:53:44 +1000

    Third commit
    Now with ethernet frame!

--- main.h
+++ main.h
@@ -4,13 +4,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include <net/ethernet.h>
 #include <netinet/udp.h>
 #include <netinet/ip.h>
+#include <net/if.h>
+#include <linux/if.h>
+#include <linux/if_packet.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 #define SERVER "127.0.0.1"
+#define IP4_HDRLEN 20
 
 /* calculate checksum routine */
 unsigned short CalcIPChecksum(unsigned short *addr, unsigned int count);
